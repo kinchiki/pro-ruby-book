eeba3cf37b9b4fc1ac26a2e98533524b1d5486eb
Akito Mizuno <mznakt@gmail.com>
2018-03-26 01:48:21 +0900

    implemet logic

--- lib/Gate.rb
+++ lib/Gate.rb
@@ -1,12 +1,26 @@
 class Gate
+  STATIONS = %i[umeda juso mikuni].freeze
+  FARES = [150, 190]
+
   def initialize(name)
     @name = name
   end
 
   def enter(ticket)
+    ticket.stamp(@name)
   end
 
   def exit(ticket)
-    true
+    fare = calc_fare(ticket)
+    fare <= ticket.fare
+  end
+
+  private
+
+  def calc_fare(ticket)
+    from = STATIONS.index(ticket.stamped_at)
+    to = STATIONS.index(@name)
+    distance = to - from
+    FARES[distance - 1]
   end
 end
