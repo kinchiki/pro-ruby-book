b62e40ce62d5c45159a11f34fefc1ae73a1a1d88
Akito Mizuno <mznakt@gmail.com>
2018-03-31 21:57:08 +0900

    implement WordSynth

--- test/word_synth_test.rb
+++ test/word_synth_test.rb
@@ -4,7 +4,21 @@ require_relative '../lib/effects'
 
 class WordSynthTest < Minitest::Test
   def test_play
-    assert WordSynth
-    assert Effects
+    synth = WordSynth.new
+    assert_equal('Ruby is fun!', synth.play('Ruby is fun!'))
+  end
+
+  def test_play_wiht_reverse
+    synth = WordSynth.new
+    synth.add_effect(Effects.reverse)
+    assert_equal('ybuR si !nuf', synth.play('Ruby is fun!'))
+  end
+
+  def test_play_wiht_many_effects
+    synth = WordSynth.new
+    synth.add_effect(Effects.echo(2))
+    synth.add_effect(Effects.loud(3))
+    synth.add_effect(Effects.reverse)
+    assert_equal('!!!YYBBUURR !!!SSII !!!!!NNUUFF', synth.play('Ruby is fun!'))
   end
 end
